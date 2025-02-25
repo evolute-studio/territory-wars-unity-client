e7e9ed75628cbbbda1a21d0b8fec6a42740c4db5
tokyobladee <124399165+tokyobladee@users.noreply.github.com>
2025-02-25 19:26:48 +0200

    tileAssetsObject updated

---
+++ Assets/TerritoryWars/General/CharactersManager.cs
@@ -0,0 +1,7 @@
+﻿namespace TerritoryWars.General
+{
+    public class CharactersManager
+    {
+        
+    }
+}
---
+++ Assets/TerritoryWars/Tools/CharacterInfo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace TerritoryWars.Tools
+{
+    public class CharacterInfo
+    {
+        public string CharacterName;
+        public Animator Animator;
+        public Sprite Icon;
+        public int Id;
+    }
+}
