4280a8f35709d6070c6b29419e009c2ed6b5a95f
tokyobladee <124399165+tokyobladee@users.noreply.github.com>
2025-03-01 13:58:18 +0200

    cancel game popup

---
+++ Assets/TerritoryWars/UI/CancelGamePopup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace TerritoryWars.UI
+{
+    public class CancelGamePopup : MonoBehaviour
+    {
+        public GameObject CancelGamePopupGO;
+        public Button CancelButton;
+        public Button ConfirmButton;
+
+
+        
+        
+        public void Initialization()
+        {
+            
+        }
+        
+        public void SetCancelPopupGameActive(bool active)
+        {
+            CancelGamePopupGO.SetActive(active);
+        }
+    }
+}
